afb3fb3b858b2d746c9998902a198cd15d1e4972
Sirtobey24 <37386453+Sirtobey24@users.noreply.github.com>
2019-11-12 21:49:07 -0800

    Merge branch 'master' of https://github.com/Sirtobey24/AnalysisofAlgos
     Conflicts:
    	src/Driver.java

--- src/Project2/Graphs.java
+++ src/Project2/Graphs.java
@@ -9,11 +9,5 @@ import java.util.Scanner;
  */
 public class Graphs
 {
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the number of nodes: ");
-
-    }
 }
